fd63eba2c774b214f5aa438249b4de4de27a8153
Giovanni Ballini <38838223+VandalMark@users.noreply.github.com>
2021-05-25 00:12:00 +0200

    Arrotondato come richiesto e altro

--- src/GenMatrix.java
+++ src/GenMatrix.java
@@ -4,25 +4,24 @@ import javax.xml.stream.XMLStreamException;
 //Questa classe crea matrici diverse in base ai veicoli
 public class GenMatrix {
 	
+	ArrayList<City> cities;
+	
+	public GenMatrix (ArrayList<City> cities) {
+		this.cities=cities;
+	}
+	
 	public double distanceTwoPoints(int i, int j, int k, int l) {
 		return Math.sqrt(Math.pow((i-k), 2)+Math.pow((j-l), 2));
 	}
 	 
 	public double[][] genTonatiuh() throws XMLStreamException {
 		
-		ArrayList<City> cities = new InputOutput().getData();
 		double[][] matrix = new double[cities.size()][cities.size()];
-		
-		/*for (int i=0; i<cities.size(); i++) {
-			for (int j=0; j<cities.size();j++) {
-				matrix[i][j]=0;
-			}
-		}*/
-		
 		for (int i=0; i<cities.size();i++) {
 			for (int j=0; j<cities.get(i).getLinksArray().size();j++) {
 				int temp=cities.get(i).getLinksArray().get(j);
-				double distance= distanceTwoPoints(cities.get(i).getX(),cities.get(i).getY(),cities.get(temp).getX(),cities.get(temp).getY());
+				//Arrotondato alla seconda cifra decimale, non dovrebbe causare problemi
+				double distance= Math.round((distanceTwoPoints(cities.get(i).getX(),cities.get(i).getY(),cities.get(temp).getX(),cities.get(temp).getY()))*100.0)/100.0;
 				matrix[i][temp]=distance;
 			}
 			
@@ -31,16 +30,8 @@ public class GenMatrix {
 		
 	}
 	
-	
 	public double[][] genMetztli() throws XMLStreamException{
-		ArrayList<City> cities = new InputOutput().getData();
 		double[][] matrix = new double[cities.size()][cities.size()];
-		
-		/*for (int i=0; i<cities.size(); i++) {
-			for (int j=0; j<cities.size();j++) {
-				matrix[i][j]=0;
-			}
-		}*/
 		for (int i=0; i<cities.size();i++) {
 			for (int j=0; j<cities.get(i).getLinksArray().size();j++) {
 			int temp=cities.get(i).getLinksArray().get(j);
--- src/RovinePerduteMain.java
+++ src/RovinePerduteMain.java
@@ -5,11 +5,14 @@ import javax.xml.stream.XMLStreamException;
 public class RovinePerduteMain {
 
 	public static void main(String[] args) throws XMLStreamException {
-		System.out.println("test");
-		System.out.println(new InputOutput().getData().size());
-		double[][] matrix = new GenMatrix().genMetztli();
-			for(int i = 0; i < 5; i++) {
-				for(int j = 0; j < 5; j++) {
+		
+
+		ArrayList<City> cities =new InputOutput().getData();
+		double[][] matrix = new GenMatrix(cities).genTonatiuh();
+		//double[][] matrix = new GenMatrix(cities).genMetztli()();
+		
+			for(int i = 0; i < cities.size(); i++) {
+				for(int j = 0; j < cities.size(); j++) {
 					System.out.print(matrix[i][j] + " ");
 				}
 				System.out.println("");
@@ -17,12 +20,6 @@ public class RovinePerduteMain {
 		}
 	
 		
-		/*ArrayList<Integer> test = new ArrayList<Integer>();
-		test.add(2);
-		test.add(24);
-		City city = new City(3,4,5,"banana",test );
-		city.getString();*/
-
 	}
 
 
